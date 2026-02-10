4c2ca0b3c7a7439c0518c0733d2d96ea08dca8dd
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-10 06:58:23 +0100

    Fixed bug in withdraw method

--- 05/app.py
+++ 05/app.py
@@ -2,8 +2,8 @@
 
 class BankAccount:
     def __init__(self, owner: str, balance: int=0) -> None:
-        self.owner = owner
-        self.__balance = balance
+        self.owner: str = owner
+        self.__balance: int = balance
     
     def deposit(self, amount: int) -> None:
         if amount > 0:
@@ -12,21 +12,25 @@ class BankAccount:
         else:
             print("Wpłacana kwota musi być większa od 0 PLN")
     
-    def withdraw(self, amount: str) -> str:
-        if self.__balance < amount:
-            print("Niewystarczające środki.")
+    def withdraw(self, amount: int) -> None:
+        if amount <= 0:
+            print("Wypłacana kwota musi być większa od zera!")
         else:
-            self.__balance -= amount
-            print(f"Wypłacono kwotę {amount} PLN. Obecny stan konta to {self.__balance} PLN")
+            if self.__balance < amount:
+                print("Niewystarczające środki.")
+            else:
+                self.__balance -= amount
+                print(f"Wypłacono kwotę {amount} PLN. Obecny stan konta to {self.__balance} PLN")
     
-    def get_balance(self):
+    def get_balance(self) -> None:
         print(f"Stan konta: {self.__balance} PLN")
         
 
 if __name__ == "__main__":
     account = BankAccount("John", 1000)
-    account.deposit(150)
-    account.withdraw(400)
+    account.deposit(400)
+    account.withdraw(100)
+    account.withdraw(-100)
     account.get_balance()
     
     
